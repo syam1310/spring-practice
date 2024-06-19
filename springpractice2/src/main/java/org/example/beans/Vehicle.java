4ef02b1ab4d464199d67e0d8931b079b4d0b17e2
Shyam Prakash.Patel.26018410 <spatel3@opentext.com>
2024-06-19 16:46:25 +0530

    Adding spring annotation and creation of bean

---
+++ springpractice2/src/main/java/org/example/beans/Vehicle.java
@@ -0,0 +1,16 @@
+package org.example.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ springpractice2/src/main/java/org/example/config/ProjectConfig.java
@@ -0,0 +1,22 @@
+package org.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+/*
+ * Spring @Configuration annotation is part of spring core framework.
+ * Spring Configuration annotation indicates that the class has @Bean definition method.
+ * So Spring container can process the class and generate Spring Beans to be used in the application.
+ *
+ * To tell Spring it needs to search for classes annotated with stereotype annotations,
+ * we use the @ComponentScan annotation over the configuration class.
+ *
+ * */
+
+@Configuration
+@ComponentScan(basePackages = "org.example.beans")
+public class ProjectConfig {
+
+
+}
