d57ca4186b53f3f3de7e7001db3567ec2c05f018
syam1310 <psyam0175@gmail.com>
2024-06-23 13:26:16 +0530

    Introducing the h2 db and Spring JDBC in the easy school

---
+++ h2-db easy school/src/test/java/com/school/eazyschool/EazyschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.eazyschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
